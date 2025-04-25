40985782168745456513066d03f30cb0779526be
mong9ssi <mong9ssi@naver.com>
2025-04-25 16:45:29 +0900

    lv2 List를 선언완료

---
+++ src/com/exemple/kiosk/lv1/CastellaMenu.java
@@ -0,0 +1,19 @@
+package com.exemple.kiosk.lv1;
+
+public class CastellaMenu {
+    // 속성
+    private String name;
+    private int price;
+    private String comment;
+
+    // 생성자
+    CastellaMenu (String name , int price , String commment) {
+        this.name = name;
+        this.price = price;
+        this.comment = commment;
+    }
+
+    // 기능
+
+
+}
